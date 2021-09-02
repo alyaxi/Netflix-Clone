697a522b75856542f6cf9a45f351916929fbe179
Syed Ali Uz Zaman <syedaliuzzaman@gmail.com>
2021-09-03 02:16:04 +0500

    Project Setup and Updated Nav Comp

---
+++ src/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../src/HomeScreen.css"
+import Nav from './Nav'
+export default function HomeScreen() {
+    return (
+        <div className="homescreen">
+       {/*Navbar*/}
+
+        <Nav/>
+
+       {/*Banner*/}
+       {/*Row*/}
+        </div>
+    )
+}
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBXDhSNoTRxBq4IAV6VCqmw3v_n2iqyw2o",
+    authDomain: "netflix-clone-25a30.firebaseapp.com",
+    projectId: "netflix-clone-25a30",
+    storageBucket: "netflix-clone-25a30.appspot.com",
+    messagingSenderId: "709765752141",
+    appId: "1:709765752141:web:1f6bfd26b040524ef3c304"
+  };
